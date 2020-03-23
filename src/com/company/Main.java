3e2e5a129a355d2c8dee0d7c97a5ee85043b967b
Michal <michalskrzypczak91@gmail.com>
2020-03-23 09:27:28 +0100

    working

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,23 +1,52 @@
 package com.company;
 
 import java.awt.*;
-import java.awt.datatransfer.Clipboard;
-import java.awt.datatransfer.StringSelection;
-import java.awt.event.KeyEvent;
+import java.util.*;
+import java.lang.reflect.Field;
+import java.awt.event.*;
+import javax.swing.*;
 
-public class Main {
+class RobotCharacter
+{
+    public static void main(String[] args) throws Exception {
+        JTextField textField = new JTextField(10);
 
-    public static void main(String[] args) throws AWTException {
-	// write your code here
-        String text = "Hello World";
-        StringSelection stringSelection = new StringSelection(text);
-        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
-        clipboard.setContents(stringSelection, stringSelection);
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        frame.add( textField );
+        frame.pack();
+        frame.setLocationRelativeTo( null );
+        frame.setVisible( true );
 
         Robot robot = new Robot();
-        robot.keyPress(KeyEvent.VK_CONTROL);
-        robot.keyPress(KeyEvent.VK_V);
-        robot.keyRelease(KeyEvent.VK_V);
-        robot.keyRelease(KeyEvent.VK_CONTROL);
+        while(true) {
+            typeCharacter(robot, "a");
+        }
+    }
+
+    public static void typeCharacter(Robot robot, String letter)
+    {
+        try
+        {
+            boolean upperCase = Character.isUpperCase( letter.charAt(0) );
+            String variableName = "VK_" + letter.toUpperCase();
+
+            Class clazz = KeyEvent.class;
+            Field field = clazz.getField( variableName );
+            int keyCode = field.getInt(null);
+
+            robot.delay(10000);
+
+            if (upperCase) robot.keyPress( KeyEvent.VK_SHIFT );
+
+            robot.keyPress( keyCode );
+            robot.keyRelease( keyCode );
+
+            if (upperCase) robot.keyRelease( KeyEvent.VK_SHIFT );
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
     }
 }
